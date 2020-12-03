b0f4329e6955f34e2bdfe040bb8775d458ba9a5e
DONAL MAHER - STUDENT <g00387859@gmit.ie>
2020-12-03 07:06:51 +0000

    [Dadding

---
+++ testpython.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('owid-covid-data1.csv')
+dfs =df[['date','new_cases']]
+#dfs = dfs.fillna(0)
+dfs.plot(kind='bar')
+##dfs['date'],dfs['new_cases'])
+#plt.show()
+dfs.show()
