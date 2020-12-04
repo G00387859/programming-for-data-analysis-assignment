0eabedb2eeffcb0bc79d21b368830c30413a9f81
DONAL MAHER - STUDENT <g00387859@gmit.ie>
2020-12-04 14:31:55 +0000

    updating from a different source

--- testpython.py
+++ testpython.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 df = pd.read_csv('owid-covid-data1.csv')
 dfs =df[['date','new_cases']]
 #dfs = dfs.fillna(0)
